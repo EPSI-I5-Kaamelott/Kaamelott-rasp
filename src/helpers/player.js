49a11c765e0eda37ee4cc8fede5ead0a41ee5cb5
Mathieu Nivoliez <mathieu.nivoliez@laposte.net>
2017-02-08 12:14:51 +0100

    Fix index number.

--- src/helpers/player.js
+++ src/helpers/player.js
@@ -20,8 +20,8 @@ const sounds_winless = [
 const get_random_sound = (songs = []) => {
   console.log('sound list', songs);
   if (songs.length !== 0) {
-    const index = Random.integer(0,songs.length);
-    console.log('song selected', songs[index]);
+    const index = Random.integer(0,songs.length-1);
+    console.log('song selected', songs[index], 'index', index);
     return songs[index];
   }
 
